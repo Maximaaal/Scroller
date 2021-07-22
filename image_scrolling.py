9a5e301c06ded2eba1f2128041cab00510e44a1e
Maximaaal <61837530+Maximaaal@users.noreply.github.com>
2021-07-22 17:31:07 +0200

    code cleanup + added comments

--- image_scrolling.py
+++ image_scrolling.py
@@ -2,13 +2,13 @@ from random import randint
 import pygame
 import sys
 
-
 exit = False
 
 SIZE = width, height = 800, 600
 BG_COLOR = (0, 0, 0)
 fullscreen = True
 
+# load random image
 images = []
 i = 0
 while len(images) < 97:
@@ -16,51 +16,39 @@ while len(images) < 97:
     i += 1
 print(images)
 
-# images = ["img1.png", "img2.jpeg", "img3.png", "img4.png"]
-# yPos = [-454, -1798, -1126, -1220]
-
 currentImage = 0
-# image = pygame.image.load(images[currentImage])
-# image = pygame.image.load(images[randint(0, 96)])
-image = pygame.image.load('images/img31.jpg')
+image = pygame.image.load(images[randint(0, 96)])
 
-# image2 = pygame.image.load(images[randint(0, 96)])
-
-
+# borders and background
 bgNative = pygame.image.load('bg/native_background.png')
 bgBinary = pygame.image.load('bg/binary_background.png')
-
 nativeTop = pygame.image.load('bg/native-border_top.png')
 nativeBottom = pygame.image.load('bg/native-border_bottom.png')
-
 binaryTop = pygame.image.load('bg/binary-border_top.png')
 binaryBottom = pygame.image.load('bg/binary-border_bottom.png')
 
-
+#screen setup
 clock = pygame.time.Clock()
 img_size = image.get_rect().size
 img_height = image.get_rect().height
 img_width = image.get_rect().width
 screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption('Scroller')
+
 if fullscreen:
     DISPLAYSURF = pygame.display.set_mode((1280, 2048), pygame.FULLSCREEN)
     pygame.mouse.set_visible(False)
 
-# x = randint(0, width)
+# img movement
 x = 640 - (img_width/2)
 y = 0 - img_height
 x_speed = 0
 y_speed = 20
 
-x2 = 640 - (img_width/2)
-y2 = 0 - (img_height + 500)
-x2_speed = 0
-y2_speed = 2
-
 print(img_height)
 print(img_width)
 
+# if image is too big then resize
 if img_width > 900:
     newImg_width = img_width/1.5
     newImg_width = int(newImg_width)
@@ -68,42 +56,19 @@ if img_width > 900:
     newImg_height = int(newImg_height)
     image = pygame.transform.scale(image, (newImg_width, newImg_height))
 
-# img_names = []
-# j = 0
-# while len(img_names) < 39:
-#     images.append("border/border_%d.jpg" % j)
-#     j += 1
-
-# all_imgs = []
-# for img in img_names:
-#     all_imgs[img] = pygame.image.load(img)
-
-# for img in img_names:
-#     screen.blit(all_imgs[img], (0, 0))
-
-
+# blit
 def move(x, y):
     screen.blit(bgNative, (0, 0))
-    screen.blit(bgBinary, (0, 1024))
-    
+    screen.blit(bgBinary, (0, 1024)) 
     screen.blit(image, (x, y))
-    
-    # screen.blit(binaryTop, (0, 1024))
-    
     screen.blit(nativeTop, (0, 0))
     screen.blit(nativeBottom, (0, 0))
     screen.blit(binaryBottom, (0, 0))
-    
-    # screen.blit(image2, (x2, y2))
-    
     
 while exit == False:
     
     screen.fill(BG_COLOR)
 
-#     topScreenColor = (0, 255, 0)
-#     pygame.draw.rect(screen, topScreenColor, (0,0,1280,1024))
-    
     x += x_speed
     y += y_speed
 
@@ -111,33 +76,31 @@ while exit == False:
     print("y=", y)
     print("img_height=", img_height)
 
+    # get image size
     img_size = image.get_rect().size
     img_height = image.get_rect().height
     img_width = image.get_rect().width
 
+    # place image in middle
     x = 640 - (img_width/2)
 
-    # if y >= 1200:
-    #     image2 = pygame.image.load(images[randint(0, 96)])
-
+    # after image is off screen then load next random image
     if y > 2048:
-        # y = 0 - img_height
         currentImage = randint(0, 96)
         image = pygame.image.load(images[currentImage])
         if img_width > 900:
             img_width = img_width/1.5
         y = (0 - image.get_rect().height) - 100
-        
-    
+
+    # if image is too big then resize
     if img_width > 900:
         newImg_width = img_width/1.5
         newImg_width = int(newImg_width)
         newImg_height = img_height/1.5
         newImg_height = int(newImg_height)
         image = pygame.transform.scale(image, (newImg_width, newImg_height))
-    # if currentImage >= 3:
-    #     currentImage = 0        
-    
+
+    # esc killswitch
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit = True
@@ -145,11 +108,9 @@ while exit == False:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-                
-                
+                       
     pygame.display.update()
     clock.tick(30)
     
-
 pygame.quit()
 sys.exit()
