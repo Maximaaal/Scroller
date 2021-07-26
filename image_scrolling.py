7b5fb558905558921f6d0b79640b9e20094e18e7
Maximaaal <61837530+Maximaaal@users.noreply.github.com>
2021-07-26 22:34:56 +0200

    bg fps test

--- image_scrolling.py
+++ image_scrolling.py
@@ -60,7 +60,7 @@ if img_width > 900:
 # blit
 def move(x, y):
     # screen.blit(bgNative, (0, 0))
-    screen.blit(bgBinary, (0, 1024)) 
+    # screen.blit(bgBinary, (0, 1024)) 
     screen.blit(binaryTop, (0, 1024))
     screen.blit(image, (x, y))
     screen.blit(nativeTop, (0, 0))
